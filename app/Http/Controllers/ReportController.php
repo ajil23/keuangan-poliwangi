61435a019848020c0f8e9dd64b5a6d958bdf1ce2
ajil23 <azrilpraya@gmail.com>
2023-12-22 09:38:19 +0800

    report view

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pelaksanaan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+
 
 class ReportController extends Controller
 {
     public function index(){
-        return view('backend.report.view_report');
-    }
+        // Menampilkan data sesuai dengan user yang sedang login
+        if(Auth::user()->id=='1'){
+           $data = Pelaksanaan::all();
+       } else {
+           $user = Auth::user()->id;
+           $data = Pelaksanaan::where('user_id', $user)->get();
+       }
+       return view('backend.report.view_report', ['data' => $data]);
+   }
 }
